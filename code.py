ab43d998e4f1a995bf6145ab800673f7fdfc082f
yusufborham <yusufborham@gmail.com>
2024-08-20 15:08:10 +0300

    i finished servo and ultrasonic code

---
+++ code.py
@@ -0,0 +1,29 @@
+from gpiozero import DistanceSensor 
+from gpiozero import Servo
+from datetime import datetime
+
+def getTime():
+    return float(str(datetime.now().time()).split(':')[2])
+
+sensor = DistanceSensor(echo=15,trigger=16)
+servo = Servo(5)
+opened = 0
+start = 0
+while True:
+    inputCommand = input()
+    if inputCommand =="on" :
+        servo.max()
+        opened = 1
+    elif inputCommand=="off":
+        servo.min()
+        opened = 0
+    if sensor.distance < 0.1 : 
+        if not start and opened :
+            start = 1
+            time_when_door_closed = getTime()
+            if opened and getTime() - time_when_door_closed > 10.0:
+                servo.min()
+                opened = 0
+                start = 0
+                
+
